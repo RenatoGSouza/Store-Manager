4cd679e9077b7caff5dbdaea1a75b4188183eb76
Renato Graça <renatomacedo1995@gmail.com>
2021-09-10 16:50:57 -0300

    test para verificar se esta sendo inserido dados no bd

--- test/unit/models.test.js
+++ test/unit/models.test.js
@@ -1,5 +1,6 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
+
 const { MongoClient } = require('mongodb');
 const { MongoMemoryServer } = require('mongodb-memory-server-core');
 
@@ -7,31 +8,32 @@ const productsModel = require('../../models/productsModel');
 const mongoConnection = require('../../models/connection');
 
 const DB_NAME = 'StoreManager';
-let connectionMock;
 
 describe('Insere um novo produto ao BD', () => {
+  let connectionMock;
+  
   const newProduct = {
     name: 'Produto Silva',
     quantity: 10,
   };
 
-  // before(async () => {
-  //   const DBServer = new MongoMemoryServer();
-  //   const URLMock = await DBServer.getUri();
+  before(async () => {
+    const DBServer = new MongoMemoryServer();
+    const URLMock = await DBServer.getUri();
 
-  //   connectionMock = await MongoClient
-  //     .connect(URLMock, {
-  //       useNewUrlParser: true,
-  //       useUnifiedTopology: true,
-  //     })
-  //     .then((conn) => conn.db(DB_NAME));
+    connectionMock = await MongoClient
+      .connect(URLMock, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((conn) => conn.db(DB_NAME));
     
-  //   sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
-  // });
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+  });
 
-  // after(() => {
-  //   mongoConnection.connection.restore();
-  // });
+  after(() => {
+    mongoConnection.connection.restore();
+  });
 
   describe('Quando o product é inserido com sucesso', () => {
     it('retorna um objeto', async () => {
@@ -44,6 +46,12 @@ describe('Insere um novo produto ao BD', () => {
       const response = await productsModel.create(newProduct);
   
       expect(response).to.have.a.property('id');
+    });
+
+    it('deve existir um product com o name cadastrado!', async () => {
+      await productsModel.create(newProduct);
+      const productCreated = await connectionMock.collection('products').findOne({ name: newProduct.name });
+      expect(productCreated).to.be.not.null;
     });
   });
 });
