53f25a162b4c553d0d759d911acbe55b1b4f67f9
Renato Graça <renatomacedo1995@gmail.com>
2021-09-11 17:08:54 -0300

    adiciona produto ao bd

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,9 +1,12 @@
 const productModel = require('../models/productsModel');
+const { isValidProduct } = require('../services/productsService');
 
 const create = async (req, res) => {
   const { name, quantity } = req.body;
-  const { id } = await productModel.create(name, quantity);
-  res.status(200).json({ id, name, quantity });
+  const result = await isValidProduct(name, quantity);
+  if (result.err) { return res.status(422).json(result); }
+  const { id: _id } = await productModel.create(name, quantity);
+  return res.status(201).json({ _id, name, quantity });
 };
 
 module.exports = {
--- services/productsService.js
+++ services/productsService.js
@@ -1,11 +1,51 @@
-const valueIsBlank = (value) => (!value);
-const code = 400;
-const isValidProduct = (name, quantity) => {
+const { connection } = require('../models/connection');
+
+const errorLength = { 
+  err: {
+  message: '"name" length must be at least 5 characters long',
+  code: 'invalid_data', 
+  },
+};
+
+const productExist = {
+  err: { 
+    message: 'Product already exists', 
+    code: 'invalid_data', 
+  }, 
+};
+
+const valueQuantityNotValid = {
+  err: { 
+    message: '"quantity" must be larger than or equal to 1', 
+    code: 'invalid_data', 
+  }, 
+};
+
+const typeQuantityNotValid = {
+  err: { 
+    message: '"quantity" must be a number', 
+    code: 'invalid_data', 
+  }, 
+};
+
+const validValueQuantity = (value) => ((value <= 0));
+const validTypeQuantity = (value) => (typeof value !== 'number');
+const valueLengthIsValid = (value, min) => ((!value) || (value.length < min));
+
+const productExists = async (name) => {
+  const { name: namedb } = await connection()
+    .then((db) => db.collection('products').findOne({ name })) || false;
+  if (namedb === name) { return true; } 
+  return false;
+};
+
+const isValidProduct = async (name, quantity) => {
   switch (true) {
-    case (valueIsBlank(name)): return { err: { message: 'Dados inválidos', code } };
-    case (valueIsBlank(quantity)): return { err: { message: 'Dados inválidos', code } };
-    default:
-      return true;
+    case (valueLengthIsValid(name, 5)): return errorLength;
+    case (validValueQuantity(quantity)): return valueQuantityNotValid;
+    case (validTypeQuantity(quantity)): return typeQuantityNotValid; 
+    case (await productExists(name)): return productExist;
+    default: return false;
   }
 };
 
